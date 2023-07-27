c5e87778584daf2179498c44a38a598b7ff7c4bf
kayla <kayladatte@gmail.com>
2023-07-26 19:24:59 -0700

    created render function and completed init and write file functions

--- index.js
+++ index.js
@@ -1,57 +1,84 @@
 // import packages
-const fs = require('fs')
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-const shapes = require('./lib/shapes')
-const Triangle = shapes.Triangle
-const Circle = shapes.Circle
-const Square = shapes.Square
+const shapes = require("./lib/shapes");
+const Triangle = shapes.Triangle;
+const Circle = shapes.Circle;
+const Square = shapes.Square;
+const SVG = shapes.SVG;
 
 const questions = [
-        {
-            type: 'input',
-            message: 'Please chose a name for your logo (no more than 3 characters',
-            name: 'text',
-            validate: function (input) { 
-                return input.length > 3
-              }
-        },
-        {
-            type: "input",
-            message: 'What color would you like the text to be?',
-            name: 'textColor',
-        },
-        {
-            type: 'list',
-            message: 'What shape would you like your logo to be?',
-            name: 'shape',
-        },
-        {
-            type: 'input',
-            message: 'What color would you like the background of the shape to be?',
-            name: 'shapeColor',
-        },
-    ]
-// // prompt user for text (no more than three char), shape, color of text, color of shape
-// function init() { 
-// // ask the user questions
-//     inquirer
-//     .prompt(questions)
-//     // we need to save the answers, maybe as a one object
-//     .then((answers) => {
-//      // console.log(answers)
-//      let logo = generateLogo(answers)        
-//      writeToFile('./examples/logo.svg', logo)
-//     }).catch((error) => {
-//         console.log(error);
-//     });
-    
-// }
+  {
+    // // prompt user for text (no more than three char), shape, color of text, color of shape
+    type: "input",
+    message: "Please chose a name for your logo (no more than 3 characters)",
+    name: "text",
+    validate: function (input) {
+      return input.length <= 3;
+    },
+  },
+  {
+    type: "input",
+    message: "What color would you like the text to be?",
+    name: "textColor",
+  },
+  {
+    type: "list",
+    message: "What shape would you like your logo to be?",
+    name: "shape",
+    choices: ["Triangle", "Circle", "Square"],
+  },
+  {
+    type: "input",
+    message: "What color would you like the background of the shape to be?",
+    name: "shapeColor",
+  },
+];
+
+function renderLogo(text, textColor, shape, shapeColor){
+    let logo = '';
+    if (shape === 'Triangle'){
+        let triangleLogo = new Triangle(shapeColor)
+        let triangleSvg = new SVG()
+        triangleSvg.setText(text, textColor)
+        triangleSvg.setShape(triangleLogo)
+        logo = triangleSvg.render()
+    }else if(shape === "Circle"){
+        let circleLogo = new Circle(shapeColor)
+        let circleSvg = new SVG()
+        circleSvg.setText(text, textColor)
+        circleSvg.setShape(circleLogo)
+        logo = circleSvg.render()
+    }else{// the user chose square
+        let squareLogo = new Square(shapeColor)
+        let squareSvg = new SVG()
+        squareSvg.setText(text, textColor)
+        squareSvg.setShape(squareLogo)
+        logo = squareSvg.render()
+    }
+    return logo
+}
+
+function init() {
+
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      // console.log(answers)
+      let genLogo = renderLogo(answers.text, answers.textColor, answers.shape, answers.shapeColor)      
+      writeToFile("./examples/logo.svg", genLogo);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 // // once answers are received, create SVG file
 //     // write file
-// function writeToFile(fileName, answers) {
-//     fs.writeFile(fileName, answers, (err) => 
-//     err ? console.error(err) : console.log('yay!'))
-// }
+function writeToFile(fileName, genLogo) {
+  fs.writeFile(fileName, genLogo, (err) =>
+    err ? console.error(err) : console.log("yay!")
+  );
+}
 
-// init()
+init();
